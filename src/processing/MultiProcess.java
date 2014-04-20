ec76db616c9c8722d8ceda3b0a04a382663c6c21
Matthew <matthewwebb65@yahoo.com>
2014-04-19 20:06:37 -0500

    finished processing

--- src/processing/MultiProcess.java
+++ src/processing/MultiProcess.java
@@ -2,15 +2,34 @@ package processing;
 
 public class MultiProcess implements Runnable {
 
+    public static void main(String[] args) {
+        ProcessAction pa1 = new ProcessAction() {
+            @Override
+            public void execute() {
+                System.out.println("pa1");
+            }
+        };
+        ProcessAction pa2 = new ProcessAction() {
+            @Override
+            public void execute() {
+                System.out.println("pa2");
+            }
+        };
+        new MultiProcess(pa1, pa2).run();
+    }
+
     private final ProcessAction[] actions;
+    public final boolean dualprocess;
 
     public MultiProcess(ProcessAction pa1, ProcessAction pa2) {
+        dualprocess = true;
         actions = new ProcessAction[2];
         actions[0] = pa1;
         actions[1] = pa2;
     }
 
     public MultiProcess(ProcessAction[] pas) {
+        dualprocess = false;
         if (pas.length > 2) {
             actions = pas;
         } else {
@@ -21,10 +40,10 @@ public class MultiProcess implements Runnable {
     @Override
     public void run() {
         if (actions != null) {
-            if (actions.length >= 2) {
-                
-            } else {
-                actions[0].execute();
+            for (ProcessAction a : actions) {
+                if (a != null) {
+                    a.execute();
+                }
             }
         }
     }
--- src/processing/ProcessAction.java
+++ src/processing/ProcessAction.java
@@ -1,5 +1,14 @@
 package processing;
 
+import info.InfoSet;
+
 public abstract class ProcessAction {
+
+    public InfoSet info = null;
+
     public abstract void execute();
+    
+    public void putInfoSet(InfoSet i) {
+        info = i;
+    }
 }
