ad05a0e87918c184394d565e7f6adcbedd94e66a
Matthew <matthewwebb65@yahoo.com>
2014-04-27 16:55:20 -0500

    updated file manegement

--- src/system/filesystem/FileGroup.java
+++ src/system/filesystem/FileGroup.java
@@ -1,18 +1,58 @@
 package system.filesystem;
 
-public class FileGroup {
+import java.io.IOException;
+
+public final class FileGroup {
 
     private final File[] files;
+    private boolean createIfNotExist = false;
+    private final int head;
 
-    public FileGroup(File... files) {
-        this.files = files;
+    public boolean allReadable = false;
+    public boolean allWritable = false;
+    public boolean allExecutable = false;
+
+    public FileGroup(File[] f, boolean create) {
+        files = f;
+        createIfNotExist = create;
+        head = -1;
     }
-    
-    public void validate() {
-        
+
+    public FileGroup(File[] f, boolean create, int header) {
+        files = f;
+        createIfNotExist = create;
+        head = header;
     }
-    
-    private void validate0() {
-        
+
+    public void validate() throws IOException {
+        update0();
+        for (int i = 0; i < files.length; i++) {
+            if (createIfNotExist && files[i].exists()) {
+                files[i].createNewFile();
+            }
+
+            if (head != -1 && i == head) {
+                //verify header
+            }
+        }
+    }
+
+    private void update0() {
+        allReadable = true;
+        allWritable = true;
+        allExecutable = true;
+        for (File f : files) {
+            if (!f.canRead()) {
+                allReadable = false;
+            }
+
+            if (!f.canWrite()) {
+                allWritable = false;
+            }
+
+            if (!f.canExecute()) {
+                allExecutable = false;
+            }
+        }
     }
 }
