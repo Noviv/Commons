318f0147adf089e9925dad50a045fb9248bd09c5
Matthew Webb <matthewwebb65@yahoo.com>
2014-05-16 13:22:42 -0500

    tree update

---
+++ src/datastructures/TernaryTree.java
@@ -0,0 +1,17 @@
+package datastructures;
+
+public class TernaryTree<V extends Object> {
+    private TreeNode on;
+    
+    public TernaryTree() {
+        
+    }
+    class TreeNode {
+        private int nodeHeight;
+        private TreeNode subL, mid, subR;
+        
+        public void nodeAdd(TreeNode obj) {
+            
+        }
+    }
+}
